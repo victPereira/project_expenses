59a7c0ed7d3bc05305dc1a841bb95c90e0dea71c
Victor Pereira <vic.torpereira@hotmail.com>
2023-01-09 07:52:31 +0000

    Primeira parte do aplicativo de despesas

---
+++ expenses/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          SizedBox(
+            child: Card(
+              color: Colors.orange,
+              elevation: 5,
+              child: Text('Gráfico'),
+            ),
+          ),
+          TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
